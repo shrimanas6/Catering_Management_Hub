041bc86552df1a3af7880525a5ebb535685c4ea6
Shriman A S <shrimanas6@users.noreply.github.com>
2024-12-28 22:13:29 +0530

    Initial commit

---
+++ catering-hub/src/main/java/com/catering/catering_hub/CateringHubApplication.java
@@ -0,0 +1,13 @@
+package com.catering.catering_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CateringHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CateringHubApplication.class, args);
+	}
+
+}
---
+++ catering-hub/src/test/java/com/catering/catering_hub/CateringHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catering.catering_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CateringHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
